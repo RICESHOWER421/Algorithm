97284239bd9745a7503a954ce13e8a2f4ccf4b02
riceshower0421 <ricesbower@petalmail.com>
2024-09-08 11:05:50 +0800

    leetcode每日一题

--- leetcode每日一题/977 有序数组的平方/main.cpp
+++ leetcode每日一题/977 有序数组的平方/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
+//第一种方法，直接排序，时间复杂度O(NlogN)
+//vector<int> sortedSquares(vector<int>& nums) {
+//    vector<int> ret;
+//    for (auto& x : nums) ret.push_back(x * x);
+//    sort(ret.begin(), ret.end());
+//    return ret;
+//}
+
+//第二种方法，双指针，时间复杂度O(N)
 vector<int> sortedSquares(vector<int>& nums) {
     vector<int> ret;
-    for (auto& x : nums) ret.push_back(x * x);
-    sort(ret.begin(), ret.end());
-    return ret;
+	int left = 0, right = nums.size() - 1;
+	while (left <= right) {
+		if (abs(nums[left]) < abs(nums[right])) {
+			ret.push_back(nums[right] * nums[right]);
+			--right;
+		}
+		else {
+			ret.push_back(nums[left] * nums[left]);
+			++left;
+		}
+	}
+
+	reverse(ret.begin(), ret.end());
+	return ret;
 }
 
 int main() {
