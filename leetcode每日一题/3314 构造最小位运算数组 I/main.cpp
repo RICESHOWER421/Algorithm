6a33329248f535c92eec448a41c67303cba615c1
riceshower0421 <ricesbower@petalmail.com>
2026-01-20 18:24:28 +0800

    leetcode每日一题

---
+++ leetcode每日一题/3314 构造最小位运算数组 I/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 2) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            int tmp = nums[i], cnt = 0;
+            while (tmp && (tmp & 1)) {
+                tmp >>= 1;
+                ++cnt;
+            }
+            ans.push_back(nums[i] - pow(2, cnt - 1));
+        }
+        return ans;
+    }
+};
+
+int main() {
+
+	return 0;
+}
