99fd1ec605416adffe09158513f7dc2007da8dd1
riceshower0421 <ricesbower@petalmail.com>
2026-01-25 20:37:21 +0800

    leetcode每日一题

---
+++ leetcode每日一题/1984 学生分数的最小差值/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDifference(vector<int>&nums, int k) {
+        if (k == 1) return 0;
+
+        sort(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.end());
+        int min_num = INT_MAX;
+        for (int i = 0; i <= nums.size() - k; i++)
+            min_num = min(min_num, nums[i] - nums[i + k - 1]);
+        return min_num;
+    }
+}; 
+
+int main() {
+
+	return 0;
+}
