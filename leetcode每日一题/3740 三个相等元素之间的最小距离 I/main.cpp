2d42f22af0086aa41f0033f210159b73971402d7
riceshower0421 <ricesbower@petalmail.com>
2026-04-10 00:29:03 +0800

    leetcode每日一题

---
+++ leetcode每日一题/3740 三个相等元素之间的最小距离 I/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size(), ans = INT_MAX;
+        vector<vector<int>> idx(n + 1);
+        for (int i = 0; i < n; i++) {
+            auto& x = idx[nums[i]];
+            if (x.size() < 3) x.push_back(i);
+            else if (x.size() == 3) {
+                x[0] = x[1];
+                x[1] = x[2];
+                x[2] = i;
+            }
+            if (x.size() == 3) ans = min(ans, abs(x[0] - x[1]) + abs(x[1] - x[2]) + abs(x[2] - x[0]));
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
+
+
+int main() {
+
+	return 0;
+}
